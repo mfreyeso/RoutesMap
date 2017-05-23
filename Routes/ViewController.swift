5c34b2e844ea8c186a24c6880cad6f3d821544f3
Mario Reyes Ojeda <mfreyesojeda@gmail.com>
2017-05-22 22:32:05 -0500

    Points with Anotation and Items

--- Routes/ViewController.swift
+++ Routes/ViewController.swift
@@ -7,17 +7,48 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    private var source: MKMapItem!
+    private var destination: MKMapItem!
+    
+    @IBOutlet weak var mapOutlet: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mapOutlet.delegate = self
+        
+        var pointCoordinates = CLLocationCoordinate2D(latitude: 6.255509, longitude: -75.580916)
+        var pointPlace = MKPlacemark(coordinate: pointCoordinates, addressDictionary: nil)
+        
+        source = MKMapItem(placemark: pointPlace)
+        source.name = "Home"
+        
+        pointCoordinates = CLLocationCoordinate2D(latitude: 6.274796, longitude: -75.591388)
+        pointPlace = MKPlacemark(coordinate: pointCoordinates, addressDictionary: nil)
+        
+        destination = MKMapItem(placemark: pointPlace)
+        destination.name = "College"
+        
+        self.putPlaceItem(place: source!)
+        self.putPlaceItem(place: destination!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func putPlaceItem(place: MKMapItem) {
+        let handlerPoint = MKPointAnnotation()
+        handlerPoint.coordinate = place.placemark.coordinate
+        handlerPoint.title = place.name
+        mapOutlet.addAnnotation(handlerPoint)
+    
     }
 
 
